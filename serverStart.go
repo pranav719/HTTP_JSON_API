b176b54de372b451445581378156fa24ace8e8ea
pranav719 <56388551+pranav719@users.noreply.github.com>
2020-12-20 15:55:12 +0530

    Update serverStart.go

--- serverStart.go
+++ serverStart.go
@@ -1,163 +1,122 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"strings"
 	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func receive(w http.ResponseWriter, r *http.Request) {
 	//timestamp := time.Now()
 
-	if !strings.Contains(r.URL.Path, "/users") {
-		http.NotFound(w, r)
-		return
-	}
-	else{
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	if strings.Contains(r.URL.Path, "/users") {
 
-	// Connect to MongoDB
-	//ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-
-	if err != nil {
-		http.Error(w, "some error in connecting",0)
-		return
-	}
-
-	// Check the connection
-	err = client.Ping(context.TODO(), nil)
-
-	if err != nil {
-		http.Error(w, "some error in ping",0)
-		return
-	}
-
-	//fmt.Println("Connected to MongoDB!")
-
-	Collection := client.Database("test").Collection("test1")
-
-	switch r.Method {
-	case "GET":
-		id := strings.SplitN(r.URL.Path, "/", 3)[2]
-		d , err:= getUserData(id)
-		if err!= nil{
-			http.Error(w, "Some Error Occureeeed", 5000)
-			break
-		}
-		fmt.Printf("data for id %d send\n", id)
-		/*for k, v := range r.URL.Query() {
+		switch r.Method {
+		case "GET":
+			id := strings.SplitN(r.URL.Path, "/", 3)[2]
+			d, err := getUserData(id)
+			if err != nil {
+				http.Error(w, "Some Error Occureeeed", 5000)
+				break
+			}
+			fmt.Printf("data for id %d send\n", id)
+			/*for k, v := range r.URL.Query() {
 			fmt.Printf("%s: %s\n", k, v)
-		}*/
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		json.NewEncoder(w).Encode(d)
-	case "POST":
-		var d User
-		/*reqBody, err := ioutil.ReadAll(r.Body)
-		if err != nil {
+			}*/
+			w.Header().Set("Content-Type", "application/json; charset=utf-8")
+			json.NewEncoder(w).Encode(d)
+		case "POST":
+			var d User
+			/*reqBody, err := ioutil.ReadAll(r.Body)
+			if err != nil {
 			log.Fatal(err)
-		}*/
-		if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
-			http.Error(w, "Some Error Occureeeed", 5000)
-			return
-		}
-		str ,err := insertUserData(d)
-		if err != nil{
-			http.Error(w, "some error in insertion", 5000)
-			return
-		}
-		fmt.Printf("\n", d)
-		//data := "Hello Bhai sab badiya"
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		json.NewEncoder(w).Encode(str)
-	default:
-		w.WriteHeader(http.StatusNotImplemented)
-		w.Write([]byte(http.StatusText(http.StatusNotImplemented)))
-	}
-}
-
-if !strings.Contains(r.URL.Path, "/contact") {
-		http.NotFound(w, r)
-		return
-	}
-	else{
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-
-	// Connect to MongoDB
-	//ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-
-	if err != nil {
-		http.Error(w, "some error in connecting",0)
-		return
-	}
-
-	// Check the connection
-	err = client.Ping(context.TODO(), nil)
-
-	if err != nil {
-		http.Error(w, "some error in ping",0)
-		return
-	}
-
-	//fmt.Println("Connected to MongoDB!")
-
-	Collection2 := client.Database("test").Collection("contactData")
-
-	switch r.Method {
-	case "GET":
-		/*id := strings.SplitN(r.URL.Path, "/", 3)[2]
-		d , err:= getUserData(id)
-		if err!= nil{
-			http.Error(w, "Some Error Occureeeed", 5000)
-			break
-		}*/
-		var userid string
-		var ct time
-		for k, v := range r.URL.Query() {
-			//fmt.Printf("%s: %s\n", k, v)
-			if(k="userid")userid = v
-			else if(k ="contacttimestamp") ct=v
-			else{
-				http.Error(w, "incorrect variable naming", 0)
+			}*/
+			if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
+				http.Error(w, "Some Error Occureeeed", 5000)
 				return
 			}
+			str, err := insertUserData(d)
+			if err != nil {
+				http.Error(w, "some error in insertion", 5000)
+				return
+			}
+			fmt.Printf("\n", d)
+			//data := "Hello Bhai sab badiya"
+			w.Header().Set("Content-Type", "application/json; charset=utf-8")
+			json.NewEncoder(w).Encode(str)
+		default:
+			w.WriteHeader(http.StatusNotImplemented)
+			w.Write([]byte(http.StatusText(http.StatusNotImplemented)))
 		}
+	} else if strings.Contains(r.URL.Path, "/contact") {
 
-		contactTime = time.Parse("2020-10-2", ct)
-		victims , err:= getContactData(userid, contactTime)
-		if err!= nil{
-			http.Error(w, "Error in retreving data", 0)
-			return
-		}
-
-
-		fmt.Printf("Possible Covid victims can be %+v\n", victims)
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		json.NewEncoder(w).Encode(victims)
-	case "POST":
-		var d contactData
-		/*reqBody, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Fatal(err)
-		}*/
-		if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
+		switch r.Method {
+		case "GET":
+			/*id := strings.SplitN(r.URL.Path, "/", 3)[2]
+			d , err:= getUserData(id)
+			if err!= nil{
 			http.Error(w, "Some Error Occureeeed", 5000)
-			log.Fatal(err)
+			break
+			}*/
+			var userid string
+			var contactTime time.Time
+			for k, v := range r.URL.Query() {
+				//fmt.Printf("%s: %s\n", k, v)
+				if k == "userid" {
+					userid = v[0]
+				} else if k == "contacttimestamp" {
+					contactTime, _ = time.Parse("2020-10-2", v[0])
+				} else {
+					http.Error(w, "incorrect variable naming", 0)
+					return
+				}
+			}
+
+			//contactTime, _ := time.Parse("2020-10-2", ct)
+			victims, err := getContactData(userid, contactTime)
+			if err != nil {
+				http.Error(w, "Error in retreving data", 0)
+				return
+			}
+
+			fmt.Printf("Possible Covid victims can be %+v\n", victims)
+			w.Header().Set("Content-Type", "application/json; charset=utf-8")
+			json.NewEncoder(w).Encode(victims)
+
+		case "POST":
+			var d Contact
+			/*reqBody, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				log.Fatal(err)
+			}*/
+			if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
+				http.Error(w, "Some Error Occureeeed", 5000)
+				return
+			}
+			str, err := insertContactData(d)
+			if err != nil {
+				http.Error(w, "Some Error Occureeeed", 5000)
+				return
+			}
+			fmt.Printf("Data received and added in database\n", d)
+			//data := "Hello Bhai sab badiya"
+			w.Header().Set("Content-Type", "application/json; charset=utf-8")
+			json.NewEncoder(w).Encode(str)
+		default:
+			w.WriteHeader(http.StatusNotImplemented)
+			w.Write([]byte(http.StatusText(http.StatusNotImplemented)))
 		}
-		str ,err := insertContactData(d)
-		fmt.Printf("Data received and added in database\n", d)
-		//data := "Hello Bhai sab badiya"
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		json.NewEncoder(w).Encode(str)
-	default:
-		w.WriteHeader(http.StatusNotImplemented)
-		w.Write([]byte(http.StatusText(http.StatusNotImplemented)))
+	} else {
+		http.NotFound(w, r)
+		return
 	}
-}
 
 	/*reqBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -180,21 +139,42 @@ if !strings.Contains(r.URL.Path, "/contact") {
 
 }
 
-func getUserData(id string) (User, error){
+func getUserData(id string) (User, error) {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		return User{}, err
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		return User{}, err
+	}
+	collection := client.Database("test").Collection("test1")
+
 	filter := bson.D{{"id", id}}
 
 	var result User
-
-	err = Collection.FindOne(context.TODO(), filter).Decode(&result)
+	err = collection.FindOne(context.TODO(), filter).Decode(&result)
 	if err != nil {
 		return User{}, err
 	}
 
-	return result, nil	
+	return result, nil
 }
 
-func insertUserData(userData User) (string, error){
-	insertResult, err := Collection.InsertOne(context.TODO(), userData)
+func insertUserData(userData User) (interface{}, error) {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		return "", err
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		return "", err
+	}
+	collection := client.Database("test").Collection("test1")
+
+	insertResult, err := collection.InsertOne(context.TODO(), userData)
 	if err != nil {
 		return "", err
 	}
@@ -202,8 +182,19 @@ func insertUserData(userData User) (string, error){
 	return insertResult.InsertedID, nil
 }
 
-func insertContactData(contactData Contact) (string, error){
-	insertResult, err := Collection2.InsertOne(context.TODO(), contactData)
+func insertContactData(contactData Contact) (interface{}, error) {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		return "", err
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		return "", err
+	}
+	collection := client.Database("test").Collection("test2")
+
+	insertResult, err := collection.InsertOne(context.TODO(), contactData)
 	if err != nil {
 		return "", err
 	}
@@ -211,24 +202,36 @@ func insertContactData(contactData Contact) (string, error){
 	return insertResult.InsertedID, nil
 }
 
-func getContactData(userid string, cTime time) ([]string, error){
-
-	filter := bson.D{{
-		"contacttimestamp": bson.D{{
-			"$gt": cTime.Add(-14*time.Day),
-			"$lt": cTime
-		}},
-		userid : bson.D{{
-			"$in": {"userid1", "userid2"}
-		}}
-	}}
+func getContactData(userid string, cTime time.Time) ([]string, error) {
 
 	var results []string
 
-	// Passing bson.D{{}} as the filter matches all documents in the collection
-	cur, err := Collection2.Find(context.TODO(), filter)
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
-		return [], err
+		return results, err
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		return results, err
+	}
+	collection := client.Database("test").Collection("test2")
+
+	filter1 := bson.D{{"contacttimestamp", bson.D{{"$and",
+		[]bson.D{
+			bson.D{{"$gt", cTime.Add(-14 * 24 * time.Hour)}},
+			bson.D{{"$lt", cTime}},
+		}}},
+	}}
+	filter2 := bson.M{
+		"user1id": userid,
+		"user2id": userid,
+	}
+
+	// Passing bson.D{{}} as the filter matches all documents in the collection
+	cur, err := collection.Find(context.TODO(), filter1, filter2)
+	if err != nil {
+		return results, err
 	}
 
 	// Finding multiple documents returns a cursor
@@ -236,14 +239,17 @@ func getContactData(userid string, cTime time) ([]string, error){
 	for cur.Next(context.TODO()) {
 
 		// create a value into which the single document can be decoded
-		var elem User
-		err := cur.Decode(&elem)
+		var elem Contact
+		err = cur.Decode(&elem)
 		if err != nil {
-			return [], err
+			return results, err
 		}
 
-		if(elem.User1ID == userid) results = append(results, elem.User2ID)
-		else results = append(results, elem.User1ID)
+		if elem.User1ID == userid {
+			results = append(results, elem.User2ID)
+		} else {
+			results = append(results, elem.User1ID)
+		}
 
 	}
 
@@ -272,7 +278,7 @@ type User struct {
 }
 
 type Contact struct {
-	User1ID                string    `json:"user1id"`
-	User2ID                string    `json:"user2id"`
+	User1ID          string    `json:"user1id"`
+	User2ID          string    `json:"user2id"`
 	ContactTimestamp time.Time `json:"contacttimestamp"`
 }
